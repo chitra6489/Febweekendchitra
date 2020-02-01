0369441e2df3cb324e80b1fa224eaeb1d9b91859
chitra6489 <chitra6489@gmail.com>
2020-02-01 11:50:30 +0530

    new code added

---
+++ src/org/samp/First.java
@@ -0,0 +1,17 @@
+package org.samp;
+
+public class First {
+	
+	private void firstpgm() {
+		System.out.println("Welcome to Java Selinium course");
+		System.out.println("Week end batch");
+		System.out.println("Project class started");
+		
+
+	}
+	public static void main(String[] args) {
+		First f = new First();
+		f.firstpgm();
+	}
+
+}
